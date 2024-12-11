3997d8d26306e07a446b0df5dd62d762ca9ceba5
Rattatammanoon Prawasuk <toprattatammanoon@hotmail.com>
2024-12-11 10:18:53 +0700

    EP14 : Fix bug

--- nextjs-app/app/lib/actions.ts
+++ nextjs-app/app/lib/actions.ts
@@ -64,7 +64,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
 
 }
 
-export async function updateInvoice(id: string, formData: FormData ,prevState: State) {
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
   const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
--- nextjs-app/app/ui/invoices/edit-form.tsx
+++ nextjs-app/app/ui/invoices/edit-form.tsx
@@ -23,7 +23,7 @@ export default function EditInvoiceForm({
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
   const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
