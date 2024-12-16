afd083d413623ba9911bcfd3658b686061497e12
Rattatammanoon Prawasuk <toprattatammanoon@hotmail.com>
2024-12-16 15:31:38 +0700

    EP16 : Adding Metadata

--- nextjs-app/app/dashboard/invoices/[id]/edit/page.tsx
+++ nextjs-app/app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,6 +2,7 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
+
  
 export default async function Page(props: { params: Promise<{ id: string }> }) {
     const params = await props.params;
